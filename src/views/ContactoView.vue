485ed2aa17c7557975ee43d3b1c10e7370aef565
artmej <artmej@microsoft.com>
2023-03-26 13:59:00 -0600

    api added

--- src/views/ContactoView.vue
+++ src/views/ContactoView.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
       <h1>Gracias por contactarnos</h1>
+      <p>{{message}}</p>
     </div>
-  </template>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: ""
+    };
+  },
+  async mounted() {
+    const { text } = await (await fetch("/api/info")).json();
+    this.message = text;
+  }
+};
+</script>
